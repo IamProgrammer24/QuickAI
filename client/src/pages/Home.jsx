b22f58d2f2012512ec94911cb165d1f58e3fa01a
Brajesh <281092322419e@gmail.com>
2025-10-25 22:41:36 +0530

    completed home page after adding footer, plan and tesimonials

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import AiTools from "../components/AiTools";
+import Testimonial from "../components/Testimonial";
+import Plan from "../components/Plan";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <HeroSection/>
-      <AiTools/>
+      <HeroSection />
+      <AiTools />
+      <Testimonial />
+      <Plan/>
+      <Footer/>
     </div>
   );
 };
