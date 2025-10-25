46f401d6462465b030dc702bf54b1f84fea1d3e1
Brajesh <281092322419e@gmail.com>
2025-10-25 12:18:37 +0530

    added AI Tools setion

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import AiTools from "../components/AiTools";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <HeroSection/>
+      <AiTools/>
     </div>
   );
 };
